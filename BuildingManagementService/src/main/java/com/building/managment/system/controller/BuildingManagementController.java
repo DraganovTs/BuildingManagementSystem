b934f7e6f4d631322d5cec554dfeb0a886bf2058
DraganovTs <cvetomir_draganov@abv.bg>
2024-11-05 20:21:25 +0200

    create building controller

--- BuildingManagementService/src/main/java/com/building/managment/system/controller/BuildingManagementController.java
+++ BuildingManagementService/src/main/java/com/building/managment/system/controller/BuildingManagementController.java
@@ -1,5 +1,9 @@
 package com.building.managment.system.controller;
 
+import com.building.managment.system.model.dto.BuildingDTO;
+import com.building.managment.system.service.BuildingService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,13 +11,29 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/api/v1/management/")
 public class BuildingManagementController {
 
+    private final BuildingService buildingService;
+
+    public BuildingManagementController(BuildingService buildingService) {
+        this.buildingService = buildingService;
+    }
+
     @PostMapping("/buildings")
-    public ResponseEntity<?> createBuilding(){
-        return ResponseEntity.ok().build();
+    public ResponseEntity<BuildingDTO> createBuilding(@Valid @RequestBody BuildingDTO buildingDTO) {
+        BuildingDTO createdBuildingDTO = buildingService.createBuilding(buildingDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdBuildingDTO);
     }
 
     @GetMapping("/buildings/{id}")
-    public ResponseEntity<?> getBuildingDetails(@PathVariable  Long id){
-        return ResponseEntity.ok().build();
+    public ResponseEntity<BuildingDTO> getBuildingDetails(@PathVariable Long id) {
+        BuildingDTO fetchBuildingDTO = buildingService.getBuildingDetails(id);
+        return ResponseEntity.status(HttpStatus.OK).body(fetchBuildingDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<BuildingDTO> deleteBuilding(@PathVariable Long id) {
+        boolean isDeleted = buildingService.deleteBuilding();
+        return isDeleted
+                ? ResponseEntity.status(HttpStatus.NO_CONTENT).build()
+                : ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
 }
--- BuildingManagementService/src/main/java/com/building/managment/system/service/BuildingService.java
+++ BuildingManagementService/src/main/java/com/building/managment/system/service/BuildingService.java
@@ -1,7 +1,20 @@
 package com.building.managment.system.service;
 
+import com.building.managment.system.model.dto.BuildingDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BuildingService {
+
+    public BuildingDTO createBuilding(BuildingDTO buildingDTO) {
+        return null;
+    }
+
+    public BuildingDTO getBuildingDetails(Long id) {
+        return null;
+    }
+
+    public boolean deleteBuilding() {
+        return false;
+    }
 }
